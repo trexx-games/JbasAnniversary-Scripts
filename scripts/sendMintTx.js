c3023b0638833aea2f3e73a8d6756a4cc163c2bc
viniciushrcs <vinciushrcs@gmail.com>
2023-08-11 08:58:03 -0300

    update scripts to improve legibility and logs

--- scripts/sendMintTx.js
+++ scripts/sendMintTx.js
@@ -1,41 +1,119 @@
 const { ethers } = require('hardhat');
+const csv = require('@fast-csv/parse');
 const { LedgerSigner } = require('@anders-t/ethers-ledger');
-const { mintFunctionAbi } = require('./abi');
+const { NFTStorage } = require('nft.storage')
 const dotenv = require('dotenv');
 const fs = require('fs');
 dotenv.config();
 
-const contractAddress = process.env.UNTRADING_CONTRACT_ADDRESS;
+let holdersAddressData = new Array();
+let Contract;
+const storageClient = new NFTStorage({ token: process.env.NFT_STORAGE_API_KEY });
 
-async function main() {
-  try {
-    console.log('...NFT MINTING STARTED...')
-    const provider = new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_API_URL, Number(process.env.POLYGON_CHAIN_ID));
-    // const signer  = new LedgerSigner(provider);
-    const [signer] = await ethers.getSigners();
+async function readFile() {
+  var stream = fs.createReadStream(__dirname + '/../data/jbas-holders.csv');
 
-    const contract = new ethers.Contract(contractAddress, mintFunctionAbi, signer);
-    const parameters = {
-      'recipient': process.env.WALLET_ADDRESS,
-      'numGenerations': '10',
-      'rewardRatio': '350000000000000000',
-      'ORatio': '300000000000000000',
-      'license': '2',
-      'tokenURI': 'ipfs://bafybeieip57f53756adpylxvbgoxwhorpmy75kplkqukcgf656j4ahcajm'
-    };
-    const totalNFTsToMint = Number(process.env.AMOUNT_OF_NFTS_TO_MINT);
-    for (let i = 0; i < totalNFTsToMint; i++) {
-      const gasEstimate = await fetch('https://gasstation.polygon.technology/v2');
-      const gasEstimateJson = await gasEstimate.json();
-      const overrides = { maxPriorityFeePerGas: ethers.utils.parseUnits(String(gasEstimateJson.standard.maxPriorityFee), 'gwei'), maxFeePerGas: ethers.utils.parseUnits(String(gasEstimateJson.standard.maxFee), 'gwei')};
-      const tx = await contract.mint(...Object.values(parameters), overrides);
-  
-      fs.appendFileSync('./logs/mint_transactions_hash.csv', `${tx.hash}\n`);
-      console.log(`Minted NFT #${i + 1}: ${tx.hash}`);
-    }
-  } catch (error) {
-    console.error('An error occurred:', error);
+  csv.parseStream(stream)
+      .on('error', error => {
+          console.log(error);
+      })
+      .on('data', row => {
+          let isAddress = ethers.utils.isAddress(row[0]);
+          if (isAddress && row[0] != null && row[0] != '') {
+              holdersAddressData.push(row[0]);
+          }
+      })
+      .on('end', _ => {
+        mint();
+      });
+
+}
+
+const init = async () => {
+  const contractAddress = process.env.UNTRADING_CONTRACT_ADDRESS;
+  const abi = require('../contracts/unFacetABI.json');
+  const provider = new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_API_URL, Number(process.env.POLYGON_CHAIN_ID));
+  // const signer = new LedgerSigner(provider);
+  const [signer] = await ethers.getSigners();
+  Contract = new ethers.Contract(contractAddress, abi, signer);
+}
+
+const uploadMetadata = async () => {
+  let data = {
+      blockchain: process.env.BLOCKCHAIN_NAME,
+      department: "Collectible",
+      description: "Merch Temple",
+      image: "ipfs://" + process.env.VIDEO_IPFS_CID,
+      license: "CBE-NECR",
+      externalUrl: "https://www.japanesebornapesociety.com/",
+      media: {
+          cid: process.env.VIDEO_IPFS_CID,
+          dimensions: "1080x1080",
+          duration: 5000,
+          mimeType: "video/mp4",
+          size: 31040256,
+          url: "https://ipfs.io/ipfs/" + process.env.VIDEO_IPFS_CID
+      },
+      name: "JBAS Merch Temple (JBAST)",
+      numberOfGenerations: 10,
+      originator: process.env.JBAS_WALLET_ADDRESS,
+      originatorRewardRatio: 30,
+      sensitiveContent: false,
+      timestamp: Date.now(),
+      totalRewardRatio: 35
   }
+  const metadata = await storageClient.storeBlob(new Blob([JSON.stringify(data)]));
+  return metadata
+}
+
+
+async function mint() {
+  console.log('...NFT MINT STARTED...');
+  const parameters = {
+    recipient: process.env.JBAS_WALLET_ADDRESS,
+    numGenerations: 10,
+    rewardRatio: ethers.utils.parseUnits("0.35", "ether"),
+    ORatio: ethers.utils.parseUnits("0.30", "ether"),
+    license: 2,
+  }
+  const writeStream = fs.createWriteStream(__dirname + "/../logs/mint_transactions.csv", { flags: 'a' });
+
+  for (let i = 0; i < holdersAddressData.length; i++) {
+    try {
+        const cid = await uploadMetadata(); 
+        const tokenURI = "ipfs://" + cid;
+        const gasEstimate = await fetch('https://gasstation.polygon.technology/v2');
+        const gasEstimateJson = await gasEstimate.json();
+        const overrides = { 
+          maxPriorityFeePerGas: ethers.utils.parseUnits(String(gasEstimateJson.standard.maxPriorityFee), 'gwei'), 
+          maxFeePerGas: ethers.utils.parseUnits(String(gasEstimateJson.standard.maxFee), 'gwei')
+        };
+        const tx = await Contract.mint(...Object.values(parameters), tokenURI, overrides);
+        // const tx = await Contract.mint(...Object.values(parameters), tokenURI);
+        const receipt = await tx.wait()
+        console.log(receipt.logs)
+        let tokenID = parseInt(BigInt(receipt.logs[0].topics[1]).toString());
+        console.log("minted token #" + tokenID);
+        writeStream.write([holdersAddressData[i], tokenID, tx.hash].join(',') + '\n');
+        console.log('------------------------');
+    } catch (error) {
+      console.error('An error occurred:', error);
+      writeStream.end();
+    }
+  }
+  writeStream.end()
+  writeStream.on('finish', () => {
+    console.log('Mint finished done.')
+  }).on('error', (err) => {
+    console.log(err)
+  })
+
+  return;
+}
+
+const main = async () => {
+  await init();
+  readFile();
 }
 
 main();
--- scripts/sendTransferTx.js
+++ scripts/sendTransferTx.js
@@ -1,36 +1,79 @@
 const fs = require('fs');
-const csv = require('csv-parser');
+const csv = require('@fast-csv/parse');
+const { ethers } = require('hardhat');
 const { LedgerSigner } = require('@anders-t/ethers-ledger');
-const { transferFunctionAbi } = require('./abi');
 const dotenv = require('dotenv');
 dotenv.config();
-const contractAddress = process.env.UNTRADING_CONTRACT_ADDRESS;
 
 
-async function main() {
-    console.log('...AIR DROP STARTED...')
+let holdersAddressData = new Array();
+let Contract;
 
+async function readFile() {
+  var stream = fs.createReadStream(__dirname + '/../logs/mint_transactions.csv');
+
+  csv.parseStream(stream)
+      .on('error', error => {
+          console.log(error);
+      })
+      .on('data', row => {
+          let isAddress = ethers.utils.isAddress(row[0]);
+          if (isAddress && row[0] != null && row[0] != '') {
+              holdersAddressData.push(row);
+          }
+      })
+      .on('end', _ => {
+        airDrop();
+      });
+}
+
+const init = async () => {
+  const contractAddress = process.env.UNTRADING_CONTRACT_ADDRESS;
+  const abi = require('../contracts/unFacetABI.json');
   const provider = new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_API_URL, Number(process.env.POLYGON_CHAIN_ID));
-  const signer  = new LedgerSigner(provider);
-  const contract = new ethers.Contract(contractAddress, transferFunctionAbi, signer);
+  // const signer = new LedgerSigner(provider);
+  const [signer] = await ethers.getSigners();
+  Contract = new ethers.Contract(contractAddress, abi, signer);
+}
 
-  let tokenId = 90;
-  //getAllTokensNumbersofSome Wallet
-  const rows = [];
+const airDrop = async () => {
+  console.log('...AIR DROP STARTED...')
+  const writeStream = fs.createWriteStream(__dirname + '/../logs/transfer_transactions.csv');
+  for (let i = 0; i < holdersAddressData.length; i++) {
+    try {
+      let holder = holdersAddressData[i];
+      let recipient = holder[0];
+      let tokenId = holder[1];
+      console.log(`Transfering TokenID ${tokenId} to ${recipient}`);
+      const gasEstimate = await fetch('https://gasstation.polygon.technology/v2');
+      const gasEstimateJson = await gasEstimate.json();
+      const overrides = {
+          maxPriorityFeePerGas: ethers.utils.parseUnits(String(gasEstimateJson.fast.maxPriorityFee), 'gwei'),
+          maxFeePerGas: ethers.utils.parseUnits(String(gasEstimateJson.fast.maxFee), 'gwei')
+      };
+      // const tx = await Contract.transferFrom(process.env.JBAS_WALLET_ADDRESS, recipient, tokenId);
+      const tx = await Contract.transferFrom(process.env.JBAS_WALLET_ADDRESS, recipient, tokenId, overrides);
+      console.log(`Transfer ${tokenId} succeed #`);
+      writeStream.write([recipient, tokenId, tx.hash].join(',') + '\n');
+      console.log('------------------------');
+    } catch (error) {
+      console.log(error);
+      writeStream.end();
+    }
+  }
+  
+  writeStream.on('finish', () => {
+    console.log('AirDrop Done.')
+  }).on('error', (err) => {
+      console.log(err)
+  })
 
-  fs.createReadStream('jbas-holders.csv')
-    .pipe(csv())
-    .on('data', async (row) => {
-      await rows.push(row.address);
-    })
-    .on('end', async () => {
-      for (const row of rows) {
-        const tx = await contract.safeTransferFrom(process.env.JBAS_WALLET_ADDRESS, row, tokenId);
-        fs.appendFileSync('./logs/transfer_transactions_hash.csv', `${tx.hash}\n`);
-        console.log(`NFT #${tokenId} SENT: ${tx.hash}`);
-        tokenId++;
-      }
-    });
+  return;
+}
+
+const main = async () => {
+  await init();
+  readFile();
 }
 
 main();
