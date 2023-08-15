653434039e2ebc53866e9d4f6f78c3091d619a33
viniciushrcs <vinciushrcs@gmail.com>
2023-08-15 11:11:02 -0300

    update for mint jbas tokens

--- scripts/sendMintTxJBAS-tokens.js
+++ scripts/sendMintTxJBAS-tokens.js
@@ -1,33 +1,9 @@
 const { ethers } = require('hardhat');
-const csv = require('@fast-csv/parse');
 const { LedgerSigner } = require('@anders-t/ethers-ledger');
-const { NFTStorage } = require('nft.storage')
 const dotenv = require('dotenv');
 const fs = require('fs');
 dotenv.config();
 
-let holdersAddressData = new Array();
-let Contract;
-const storageClient = new NFTStorage({ token: process.env.NFT_STORAGE_API_KEY });
-
-async function readFile() {
-  var stream = fs.createReadStream(__dirname + '/../data/jbas-holders.csv');
-
-  csv.parseStream(stream)
-      .on('error', error => {
-          console.log(error);
-      })
-      .on('data', row => {
-          let isAddress = ethers.utils.isAddress(row[0]);
-          if (isAddress && row[0] != null && row[0] != '') {
-              holdersAddressData.push(row[0]);
-          }
-      })
-      .on('end', _ => {
-        mint();
-      });
-
-}
 
 const init = async () => {
   const contractAddress = process.env.UNTRADING_CONTRACT_ADDRESS;
@@ -37,35 +13,6 @@ const init = async () => {
   // const [signer] = await ethers.getSigners();
   Contract = new ethers.Contract(contractAddress, abi, signer);
 }
-
-const uploadMetadata = async () => {
-  let data = {
-      blockchain: process.env.BLOCKCHAIN_NAME,
-      department: "Collectible",
-      description: "Merch Temple",
-      image: "ipfs://" + process.env.VIDEO_IPFS_CID,
-      license: "CBE-NECR",
-      externalUrl: "https://www.japanesebornapesociety.com/",
-      media: {
-          cid: process.env.VIDEO_IPFS_CID,
-          dimensions: "1080x1080",
-          duration: 5000,
-          mimeType: "video/mp4",
-          size: 31040256,
-          url: "https://ipfs.io/ipfs/" + process.env.VIDEO_IPFS_CID
-      },
-      name: "JBAS Merch Temple (JBAST)",
-      numberOfGenerations: 10,
-      originator: process.env.JBAS_WALLET_ADDRESS,
-      originatorRewardRatio: 30,
-      sensitiveContent: false,
-      timestamp: Date.now(),
-      totalRewardRatio: 35
-  }
-  const metadata = await storageClient.storeBlob(new Blob([JSON.stringify(data)]));
-  return metadata
-}
-
 
 async function mint() {
   console.log('...NFT MINT STARTED...');
@@ -78,7 +25,7 @@ async function mint() {
   }
   const writeStream = fs.createWriteStream(__dirname + "/../logs/mint_transactions.csv", { flags: 'a' });
 
-  for (let i = 0; i < holdersAddressData.length; i++) {
+  for (let i = 0; i <= 20; i++) {
     try {
         //run only one time for efficiency
         // const cid = await uploadMetadata(); 
@@ -95,7 +42,6 @@ async function mint() {
         console.log(receipt.logs)
         let tokenID = parseInt(BigInt(receipt.logs[0].topics[1]).toString());
         console.log("minted token #" + tokenID);
-        writeStream.write([holdersAddressData[i], tokenID, tx.hash].join(',') + '\n');
         console.log('------------------------');
     } catch (error) {
       console.error('An error occurred:', error);
@@ -114,7 +60,7 @@ async function mint() {
 
 const main = async () => {
   await init();
-  readFile();
+  await mint();
 }
 
 main();
